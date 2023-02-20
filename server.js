034d10ea586896e1757adc194795ee3fcad8457e
Cody Cooper <cody_codster@live.com>
2023-02-19 19:00:18 -0600

    Created mongoose DB connection and updated server file.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 
@@ -7,6 +8,8 @@ const app = express();
 app.use(express.urlencoded({extended: true})); //Parses data from URL
 app.use(express.json()); //Parses data from request body payload
 
+db.once('open', () => {
 app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
+});
