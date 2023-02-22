b12c60b2765de97e3c372a56ee49a3c2c3de48f4
Cody Cooper <cody_codster@live.com>
2023-02-22 08:47:14 -0600

    Created .env file to hide credentials

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 // Wrap Mongoose around local connection to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/socialnetworkDB', {
+mongoose.connect(process.send.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
