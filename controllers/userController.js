e6479b0ca70530891d2e169f7e97df14c1a53776
Cody Cooper <cody_codster@live.com>
2023-02-23 04:59:26 -0600

    Updated getUserById route

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,4 +7,10 @@ module.exports = {
             err ? res.status(500).json(err) : res.status(200).json(docs)
         });
     },
+
+    //Get a single user by id
+    async getUserById(req, res) {
+        const user = await User.findOne({ _id: req.params.id });
+        !user ? res.status(400).json({message: `No user found with id: req.params.id`}) : res.status(200).json(user);
+    },
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { getAllUsers } = require('../../controllers/userController');
+const { getAllUsers, getUserById } = require('../../controllers/userController');
 
 router.route('/').get(getAllUsers);
+router.route('/:id').get(getUserById);
 
 module.exports = router;
